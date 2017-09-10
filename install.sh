172ead4031b0b3a35fdcb43c9bca9d51dd63e5ac
dmytro.lymarenko <dmytro.lymarenko@gmail.com>
2017-09-10 16:55:20 +0300

    Refactor deb installation

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+# Utils
+
+function install_deb {
+ 	echo -e "\e[32mInstall $1\e[0m"
+	rm "app.deb"
+	wget -O "app.deb" $2
+	sudo dpkg -i "app.deb"
+	sudo apt install -f -y
+}
+
 ################################################################################
 # INSTALL PROGRAMS                                                             #
 ################################################################################
@@ -5,19 +15,7 @@
 echo -e "\e[32mInstall programs\e[0m"
 
 # Chrome
-echo -e "\e[32mInstall Chrome\e[0m"
-
-rm google-chrome-stable_current_amd64.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt install -f -y
-rm google-chrome-stable_current_amd64.deb
+install_deb "Chrome" "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
 # Atom
-echo -e "\e[32mInstall Atom\e[0m"
-
-rm atom.deb
-wget -O atom.deb https://atom.io/download/deb
-sudo dpkg -i atom.deb
-sudo apt install -f -y
-rm atom.deb
+install_deb "Atom" "atom.deb https://atom.io/download/deb"
