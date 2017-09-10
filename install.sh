1102b7202b39e65ed9f111f6891bd2872c6fcef6
dmytro.lymarenko <dmytro.lymarenko@gmail.com>
2017-09-10 19:44:20 +0300

    Add telegram installation

--- install.sh
+++ install.sh
@@ -20,6 +20,35 @@ function install_deb {
 	rm $file_name
 }
 
+function install_from_tar_xz {
+	file_name="archive.tar.xz"
+
+	echo_green "Install $1"
+
+	if [ -f $file_name ]
+	then
+		rm $file_name
+	fi
+	wget -O $file_name $2
+
+	archive_directory="archive_directory"
+
+	if [ -d $archive_directory ]
+	then
+		rm -rf $archive_directory
+	fi
+
+	mkdir "$archive_directory"
+	mkdir "$archive_directory/$3"
+
+	tar xvfJ $file_name --directory "$archive_directory/$3" --strip-components=1
+
+	cp -R "$archive_directory/$3" "/opt/$3"
+
+	rm $file_name
+	rm -rf $archive_directory
+}
+
 ################################################################################
 # INSTALL PROGRAMS                                                             #
 ################################################################################
@@ -37,3 +66,6 @@ install_deb "Slack" "https://downloads.slack-edge.com/linux_releases/slack-deskt
 
 # Skype
 install_deb "Skype" "https://repo.skype.com/latest/skypeforlinux-64.deb"
+
+# Telegram
+install_from_tar_xz "Telegram" "https://updates.tdesktop.com/tlinux/tsetup.1.1.23.tar.xz" "telegram"
