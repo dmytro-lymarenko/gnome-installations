34060c55558b7cd14b3fbd3c1ba307bfe81d908a
dmytro.lymarenko <dmytro.lymarenko@gmail.com>
2017-09-10 16:13:10 +0300

    Add chrome installation

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-echo "It should work"
+################################################################################
+# INSTALL PROGRAMS                                                             #
+################################################################################
+
+echo -e "\e[32mInstall programs\e[0m"
+
+# Chrome
+echo -e "\e[32mInstall Chrome\e[0m"
+
+rm google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install -f -y
+rm google-chrome-stable_current_amd64.deb
