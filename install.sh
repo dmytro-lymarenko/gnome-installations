4bd03ac0349d74433cc31ab125ab36bee9c71ac6
dmytro.lymarenko <dmytro.lymarenko@gmail.com>
2017-09-10 16:27:27 +0300

    Add atom installation

--- install.sh
+++ install.sh
@@ -12,3 +12,12 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install -f -y
 rm google-chrome-stable_current_amd64.deb
+
+# Atom
+echo -e "\e[32mInstall Atom\e[0m"
+
+rm atom.deb
+wget -O atom.deb https://atom.io/download/deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install -f -y
+rm atom.deb
